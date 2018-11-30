00c8bef9ae508d4c74289678725c4cd6e84de424
vuzun <vladimir.uzun@gmail.com>
2018-11-30 12:11:00 +0000

    Current scheme should be added now

--- clscoring/ui.R
+++ clscoring/ui.R
@@ -3,11 +3,10 @@ ui <- navbarPage("Cell line scoring",
                  
                  tabPanel("Start",
                           sidebarLayout(
-                            sidebarPanel("Select tumour and cell line type:",
+                            sidebarPanel("Select tumour and data type for the machine learning model:",br(),
                                          selectInput("tum", label = "Tumour type:", 
                                                      list("Tumour cohorts" = c("UCEC", "BRCA"))),
-                                         actionButton("do_model", "Build a model"),
-                                         hr(),
+                                         br(),
                                          radioButtons("datatypeSelected", "Data types",
                                                       choices = list("Expression" = "exp",
                                                                      "Copy number" = "cn",
@@ -15,51 +14,80 @@ ui <- navbarPage("Cell line scoring",
                                                                      "Combination" = "combo"),
                                                       selected = "cn"
                                          ),
+                                         actionButton("do_model", "Build a model"),
                                          hr(),
-                                         selectInput("cl", label = "Select a CL of x type",
+                                         selectInput("cl", label = "Select a cell line to score:",
                                                      choices = c("AN3CA", "MFE-280", "JHUEM-3") ),
                                          actionButton("do_score", "Score the cell line"),
-                                         actionButton("do_score_2","Score all")#,
+                                         hr(),
+                                         br(),
+                                         tags$b("Calculate and plot all the cell line scores of this cancer type:"),br(),
+                                         actionButton("do_score_2",
+                                                      "Score and plot all")#,
                                          #hr(), hr(),
                                          
                             ),
-                            mainPanel("Tumour classifier performance - F1= ",
+                            mainPanel("Classification scheme: Default",
+                                      br(),
+                                      "Tumour classifier performance - AUC= 0.87",
                                       hr(),
                                       plotOutput("mds"),
                                       textOutput("rez"),
                                       textOutput("cl_dist"),
-                                      tableOutput("feele"),
                                       plotOutput("plotscore"))
                           )
                  ),
-                 tabPanel("Update classifications",
+                 tabPanel("Classification schemes",
                           sidebarLayout(
                             sidebarPanel(
                               fileInput("fajla",
-                                        "Upload own TCGA subtypes for the selected type",
+                                        "Upload own TCGA subtypes for the selected cancer type",
                                         accept = c("text/csv", 
                                                    "text/comma-separated-values,text/plain", ".csv")),
-                              actionButton("barcode_update","Update the barcode classification")
+                              actionButton("barcode_update","Update the classification scheme"),
+                              hr(),
+                              actionButton("download","Download the current scheme")
+                              
                             ),
-                            mainPanel("Hi!",br(),"Here you can provide own set of classifications in the format of a table of \"TCGA barcodes\"~\"class\" pairs.",br(),
-                                      "TCGA barcodes need to be in proper format. Click update, then build mdoel again?")
+                            mainPanel("You can provide a different classification scheme or download the current one.",br(),
+                                      "The classsification scheme table needs to have 2 columns:",br(),
+                                      tags$ul(
+                                        tags$li("Valid TCGA barcodes"),
+                                        tags$li("Column containing only 2 different values")
+                                      ),
+                                      
+                                      "Once the custom table is uploaded and classification updated, rebuild the model.", br(),
+                                      hr(),br(),
+                                      tags$b("Top of the current classification table:"),
+                                      br(),
+                                      tableOutput("tcga_table"))
                           )
                  ),
                  
 
                  
                  tabPanel("Cell line distances",
-                          sidebarLayout(sidebarPanel(),mainPanel(
-                          actionButton("do_dists", "Calculate dist for selected classifications and tumour set"),
-                          
-                          
-                          checkboxGroupInput("deldis",
-                                             h3("Dat"),
-                                             choices = list("Expression" = 1,
-                                                            "Copy number" = 2,
-                                                            "Mutation" = 3),
-                                             selected = c(1,2,3)
-                          )
+                          sidebarLayout(sidebarPanel(
+                            h3("Current settings"),br(),
+                            tags$b("Tumour type:"), textOutput("tum"),
+                            br(),
+                            tags$b("Classification scheme:"),textOutput("histname"),
+                            hr(),
+                            tags$b("Data type:"), textOutput("datatypeSelected")
+                            
+                            
+                          ),mainPanel("Aside from the classification model, cell line similarity to patient samples can also be helpful.",
+                                      "Here, number of patient samples that have a certain cell line as their closest cell line can be calculated for selected cancer type, data and classification scheme.",
+                          br(),
+                          actionButton("do_dists", "Count closest cell lines"),
+                          br(),br(),br(),
+                          plotOutput("plotcldist")
+                          ))
+                 ),
+                 tabPanel("About",
+                          sidebarLayout(sidebarPanel(
+
+                          ),mainPanel(
                           ))
                  )
                  
